96fa0421e38222fafe746e23400ac236746fc53e
liuchangjiang666 <llc5131025>
2017-09-15 09:31:39 +0800

    刘常江更新company内容

---
+++ src/main/java/cn/bole/pojo/CompanyInfo.java
@@ -0,0 +1,76 @@
+package cn.bole.pojo;
+
+public class CompanyInfo {
+   private Integer companyInfoId;
+   private String companyName;
+   private Integer nature;
+   private Integer scale;
+   private String industry;
+   private String introduction;
+   private String address;
+   private String location;
+   private String telphone;
+   private Integer isFamous;
+   //private Job job;
+public Integer getCompanyInfoId() {
+	return companyInfoId;
+}
+public void setCompanyInfoId(Integer companyInfoId) {
+	this.companyInfoId = companyInfoId;
+}
+public String getCompanyName() {
+	return companyName;
+}
+public void setCompanyName(String companyName) {
+	this.companyName = companyName;
+}
+public Integer getNature() {
+	return nature;
+}
+public void setNature(Integer nature) {
+	this.nature = nature;
+}
+public Integer getScale() {
+	return scale;
+}
+public void setScale(Integer scale) {
+	this.scale = scale;
+}
+public String getIndustry() {
+	return industry;
+}
+public void setIndustry(String industry) {
+	this.industry = industry;
+}
+public String getIntroduction() {
+	return introduction;
+}
+public void setIntroduction(String introduction) {
+	this.introduction = introduction;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getLocation() {
+	return location;
+}
+public void setLocation(String location) {
+	this.location = location;
+}
+public String getTelphone() {
+	return telphone;
+}
+public void setTelphone(String telphone) {
+	this.telphone = telphone;
+}
+public Integer getIsFamous() {
+	return isFamous;
+}
+public void setIsFamous(Integer isFamous) {
+	this.isFamous = isFamous;
+}
+   
+}
--- src/main/java/cn/bole/serviceImpl/CompanyServiceImpl.java
+++ src/main/java/cn/bole/serviceImpl/CompanyServiceImpl.java
@@ -1,7 +1,26 @@
 package cn.bole.serviceImpl;
 
-import cn.bole.service.CompanyService;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.bole.mapper.CompanyMapper;
+import cn.bole.pojo.Company;
+import cn.bole.service.CompanyService;
+@Service
 public class CompanyServiceImpl implements CompanyService {
+    @Autowired
+	private CompanyMapper companyMapper;
+
+	@Override
+	public List<Company> findCompanyAll() {
+		
+		return companyMapper.findCompanyAll();
+	}
+
+	
+	
+	
 
 }
